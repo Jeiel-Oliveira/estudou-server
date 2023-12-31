b5c285f683d375ebbd7bf2f2df5126a20ebb1fe5
Jeiel Alves <jeielalves25@gmail.com>
2023-12-31 16:18:18 -0300

    fix: remove all warning for scheduleServiceTests

--- schedule-service/src/test/java/com/brokengate/Project/Estudou/service/ScheduleServiceTests.java
+++ schedule-service/src/test/java/com/brokengate/Project/Estudou/service/ScheduleServiceTests.java
@@ -7,8 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import javax.swing.text.html.Option;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
